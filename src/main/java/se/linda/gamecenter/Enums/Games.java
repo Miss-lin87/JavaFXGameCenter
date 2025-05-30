52c7986da6453cc25c76c297198285698fa60765
linda <linda.ek@bredband2.com>
2025-05-30 14:42:15 +0200

    cleanup 2

--- src/main/java/se/linda/gamecenter/Enums/Games.java
+++ src/main/java/se/linda/gamecenter/Enums/Games.java
@@ -1,19 +1,25 @@
 package se.linda.gamecenter.Enums;
 
 public enum Games {
-    FLIPGAME ("3"),
-    MEMORY ("1"),
-    ROBOT ("5"),
-    RPSGAME ("4"),
-    WORDGUESSER ("2");
+    MEMORY ("1", "Memory"),
+    WORDGUESSER ("2", "Word Guesser"),
+    FLIPGAME ("3","Flip Game"),
+    RPSGAME ("4", "Rock, Paper, Scissors"),
+    ROBOT ("5", "Robot Game");
 
     String game;
+    String name;
 
-    Games(String game) {
+    Games(String game, String name) {
         this.game = game;
+        this.name = name;
     }
 
     public String getGame(){
         return game;
+    }
+
+    public String getName() {
+        return name;
     }
 }
